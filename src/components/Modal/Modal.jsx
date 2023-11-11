897faad5f20b73865e8a2e8069c0d8c73c1b047b
alina513 <130853290+alina513@users.noreply.github.com>
2023-11-11 02:09:55 +0200

    photo

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -2,6 +2,6 @@ import css from "./Modal.module.css"
 export const Modal = ({image, closeModal}) => 
 (<div className={css.overlay} onClick={closeModal}>
     <div className={css.modal}>
-     <img src={image}></img>   
+     <img src={image} alt="photo"></img>   
     </div>
 </div>)
